df7c5952b3ff9f0b106ed7c2571b98ffbdee2cae
Kowal546 <80398069+Kowal546@users.noreply.github.com>
2021-06-17 20:02:57 +0200

    Update Anty.py

--- Anty.py
+++ Anty.py
@@ -7,10 +7,10 @@ def g(prompt, vmin, vmax):
             print('Podaj liczbe.')
             continue
         if number < vmin:
-            print("Zbyt mała wartość liczby. Podaj liczbę całkowita od 1 do 10.")
+            print("Zbyt mała wartość liczby. Podaj liczbę całkowita od: ", vmin, "do: ", vmax)
             continue
         if number > vmax:
-            print("Zbyt duza wartosc liczby. Podaj liczbe calkowita od 1 do 10.")
+            print("Zbyt duza wartosc liczby. Podaj liczbę całkowita od: ", vmin, "do: ", vmax)
             continue
         break
     return number
